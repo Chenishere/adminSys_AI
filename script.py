e9a4a8e6daf0cf7fc28429db7fb0f16d69457b1e
YBOUSSERIA <yanice.bousseria@ynov.com>
2024-05-07 10:44:05 +0200

    test for linux

--- script.py
+++ script.py
@@ -1,28 +1,28 @@
 import json
 from difflib import get_close_matches
+import subprocess
 
 def load_data_ai(file_path: str) -> dict:
     with open(file_path, 'r') as file:
         data: dict = json.load(file)
         return data
-    
-
 
 def save_data_ai(file_path: str, data: dict):
-    with open(file_path, 'w')as file:
+    with open(file_path, 'w') as file:
         json.dump(data, file, indent=2) 
 
-
-def find_best_match(user_question: str, questions: list[str])-> str | None:
+def find_best_match(user_question: str, questions: list[str]) -> str | None:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
     return matches[0] if matches else None
 
-
-def get_answer_for_question(question: str, data_ai: dict)-> str | None: 
+def get_answer_for_question(question: str, data_ai: dict) -> str | None: 
     for q in data_ai["questions"]:
         if q["questions"] == question:
             return q["answer"]
-    
+
+def execute_command(command: str):
+    subprocess.run(command, shell=True)
+
 def chat_bot():
     data_ai: dict = load_data_ai('data_ai.json')
 
@@ -41,6 +41,8 @@ def chat_bot():
         if best_match:
             answer: str = get_answer_for_question(best_match, data_ai)
             print(f'Bot: {answer}')
+            if answer:
+                execute_command(answer)
         else:
             print(f'Bot: I don\'t know the answer. Can you teach me?')
             new_answer: str = input('Type the answer or "skip" to skip: ')
